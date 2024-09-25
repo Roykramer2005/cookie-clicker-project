f3766f611bf0ca56f2bc777a5925f6aa941d335c
indy brinkman <9021720@student.zadkine.nl>
2024-09-25 13:14:56 +0200

    begin gemaakt

---
+++ cookie.js
@@ -0,0 +1,52 @@
+class CookieClicker {
+    constructor() {
+        this.score = 0;
+        this.pointsPerClick = 1;
+        this.upgradeCost = 10;
+        this.upgradeMultiplier = 2;
+        this.init();
+    }
+
+    init() {
+        this.cookieElement = document.getElementById('cookie');
+        this.scoreElement = document.getElementById('score');
+        this.upgradeButton = document.getElementById('upgrade');
+
+
+        this.cookieElement.addEventListener('click', () => this.incrementScore());
+
+        this.upgradeButton.addEventListener('click', () => this.buyUpgrade());
+
+
+        this.updateUI();
+    }
+
+    incrementScore() {
+        this.score += this.pointsPerClick;
+        this.updateUI();
+    }
+
+
+    updateUI() {
+        this.scoreElement.innerText = `Score: ${this.score}`;
+        this.upgradeButton.innerText = `Koop Upgrade (kost ${this.upgradeCost} punten)`;
+
+        
+        if (this.score >= this.upgradeCost) {
+            this.upgradeButton.disabled = false;
+        } else {
+            this.upgradeButton.disabled = true;
+        }
+    }
+
+    buyUpgrade() {
+        if (this.score >= this.upgradeCost) {
+            this.score -= this.upgradeCost;
+            this.pointsPerClick += 1;
+            this.upgradeCost *= this.upgradeMultiplier;
+            this.updateUI();
+        }
+    }
+}
+
+const game = new CookieClicker();
